4833885cac3ec5545bcff8fbd2add2a28a259efd
Aishwarya <aishwaryaachar26@gmail.com>
2026-04-19 16:46:14 +0530

    SearchIn2DArray.java

---
+++ SearchIn2DArray.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class SearchIn2DArray{
+    public static void main(String[] args){
+        Scanner sc =new Scanner (System.in);
+        int rows =sc.nextInt();
+        int cols=sc.nextInt();
+        int[][]number=new int[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                number[i][j]=sc.nextInt();
+            }
+        }
+        int x=sc.nextInt();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(number[i][j]==x){
+                    System.out.print("x found at locations("+i+","+j+")");
+                }
+            }
+             System.out.println();
+        }
+    }
+}
